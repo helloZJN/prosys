5229d9ff309f8016b7e99df042bb6975c5671ee9
QPW <810863273@qq.com>
2018-06-06 11:17:23 +0800

    数据库
    数据库操作的例子（test.php）

---
+++ conn/Oracle_oci.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * 数据库的一些基本操作
+ */
+class Oracle_oci
+{
+	var $user;
+	var $pwd;
+	var $dbname;
+	var $conn;
+	function __construct($user='c##scott',$pwd='tiger',$dbname='//localhost/orcl')
+	{
+		$this->user=$user;
+		$this->pwd=$pwd;
+		$this->dbname=$dbname;
+	}
+	function conn(){
+		$this->conn = oci_connect($this->user, $this->pwd,$this->dbname);
+	}
+	function close(){
+		oci_close($this->conn);
+	}
+	function insert($query){
+		$stid = oci_parse($this->conn, $query);
+		oci_execute($stid);
+	}
+	function select($query){
+		$stid = oci_parse($this->conn, $query);
+		oci_execute($stid);
+		return $stid;
+	}
+	function update($query){
+		$stid = oci_parse($this->conn, $query);
+		oci_execute($stid);
+	}
+	function delete($query){
+		$stid = oci_parse($this->conn, $query);
+		oci_execute($stid);
+	}
+}
+
+?>
---
+++ test.php
@@ -0,0 +1,25 @@
+<?php 
+	include_once('conn/Oracle_oci.class.php');
+	$dbe=new Oracle_oci();
+	$dbe->conn();
+	if($dbe->conn){
+		//增加
+		$dbe->insert("insert into admin values('000005','5')");	
+		//更新	
+		$dbe->update("update admin set  admid='666666' where admid='000000'");
+		//删除
+		$dbe->delete('delete from admin');
+		//查找
+		$stid=$dbe->select("select * from admin");
+		while($row=oci_fetch_array($stid)){
+			foreach ($row as $value) {
+				echo "$value";
+			}
+		}
+		$dbe->close();
+	}else {
+		echo "no";
+	}
+
+
+ ?>
