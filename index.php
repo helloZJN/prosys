c7ce5f7a4746d5c76914a4c5146c048c20aee08c
ZJN <810863273@qq.com>
2018-06-06 09:10:23 +0800

    更新

--- index.php
+++ index.php
@@ -11,199 +11,22 @@
 
 		<!-- Bootstrap core CSS -->
 		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-		<style type="text/css">
-			html {
-				width: 100%;
-				height: 100%;
-			}
-			body {
-				background-color: #36679c;
-				overflow: hidden;
-			}
-			@media (max-width: 768px) {
-				body {
-					background-color: #ffffff;
-				}
-			}
-			
-			.btn:focus,
-			input:focus {
-				outline: none;
-				border: none;
-			}
-			
-			.padding-line {
-				height: 1px;
-				margin-top: 150px;
-				/*background-color: #506776;*/
-				margin-left: -5%;
-				margin-right: -5%;
-			}
-
-			.container {
-				max-width: 480px;
-				border-radius: 4px;
-				background-color: #ffffff;
-			}
-			
-			#nav-menu {
-				height: 48px;
-				max-width: 480px;
-				margin-left: -15px;
-				margin-right: -15px;
-				padding-left: 10%;
-				padding-right: 10%;
-				background-color: #fafafa;
-				border-radius: 4px;
-			}
-			#nav-menu ul li {
-				line-height: 48px;
-				text-align: center;
-				font-size: 16px;
-				margin: 0;
-				color: #868d9b;
-			}
-			#nav-menu .login {
-				width: 44%;
-			}
-			#nav-menu .divider {
-				width: 10%;
-				color: #dde1e7;
-			}
-			#nav-menu .register {
-				width: 44%;
-			}
-			#nav-menu .active {
-				color: #4d95fb;
-				border-bottom: 2px solid #4d95fb;
-			}
-
-			#login-overview,
-			#register-overview {
-				padding: 8% 0px;
-			}
-			#login-msg,
-			#register-msg {
-				/*display: none;*/
-				visibility: hidden;
-				margin-bottom: 10px;
-				/*text-indent: -999px;*/
-			}
-			.container .alert-warning {
-				border: none;
-				background-color: #fee8e8;
-				font-size: 12px;
-				color: #d15a5a;
-				padding: 10px 15px;
-				border-radius: 2px;
-				/*text-align: center;*/
-			}
-
-			.container form input {
-				height: 50px;
-				color: #cacdd3;
-				background-color: #ffffff;
-				border: 1px solid #dde1e7;
-				border-radius: 2px;
-				font-size: 16px;
-				-webkit-box-shadow: none;
-				box-shadow: none;
-				-webkit-appearance: none;
-				outline: none;
-			}
-			.container form input:hover,
-			.container form input:focus {
-				border: 1px solid #9ba1ac;
-				color: #7b8392;
-				-webkit-box-shadow: none;
-				box-shadow: none;
-				-webkit-appearance: none;
-				outline: none;
-			}
-			input:-webkit-autofill {
-				-webkit-box-shadow: 0 0 0px 1000px #ffffff inset !important;
-				-webkit-text-fill-color: #7b8392;
-			}
-			.container form input.btn {
-				background-color: #4d95fb;
-				color: #ffffff;
-				border: 0;
-			}
-			.container form input.btn:hover,
-			.container form input.btn:focus,
-			.container form input.btn:active,
-			.container form input.btn:visited {
-				background-color: #64a3fd;
-				color: #c2dafe;
-				border: none;
-				outline: none;
-			}
-			.container .forgot-password {
-				color: #868d9b;
-				font-size: 14px;
-				margin-top: -10px;
-				/*text-decoration: underline;*/
-			}
-			.container .forgot-password:hover,
-			.container .forgot-password:focus,
-			.container .forgot-password:active,
-			.container .forgot-password:visited {
-				text-decoration: none;
-			}
-			.container .form-group {
-				margin-bottom: 20px;
-				width: 100%;
-			}
-			.container .form-group:first-child {
-				margin-bottom: 0;
-			}
-			.container .form-group:nth-child(2) {
-				display: inline-block;
-			}
-
-			#login-overview {
-				/*display: none;*/
-			}
-
-			#register-overview {
-				display: none;
-				/*
-				 position: absolute;
-				 top: 0;
-				 width: 100%;
-				 margin-top: 33%;
-				 right: 100%;
-				 */
-			}
-
-			#strength {
-				display: none;
-				position: absolute;
-				font-size: 12px;
-				line-height: 18px;
-			}
-			.red {
-				color: #fb4738;
-			}
-			.low {
-				color: #fecf71;
-			}
-			.middle {
-				color: #669ae1;
-			}
-			.high {
-				color: #61d01c;
-			}
-			
-		</style>
+		<link href="css/index.css" rel="stylesheet">
+		
 	</head>
 
 	<body>
-		<!-- 用于PC端 增大上边距 -->
-		<div class="padding-line visible-md visible-lg"></div>
-		
-		<div class="container">
+
+		<div class="intro-video">
+			<video id="video" class="video" autoplay="autoplay" loop="loop" src="//qzonestyle.gtimg.cn/qz-proj/wy-pc-v2/static/img/web/top.webm">
+			</video>
+		</div>
+
+		<div align="center">
+			<img src="imgs/logo.png" height="200">
+		</div>
+
+		<div class="container" align="center">
 			<nav id="nav-menu">
 				<ul class="nav nav-pills">
 					<li class="login active" value="login">
@@ -228,26 +51,15 @@
 						</div>
 
 						<div class="form-group">
-							<input type="email" name="email" id="email" class="form-control input-lg" placeholder="请输入邮箱地址">
+							<input type="text" name="userid" id="userid" class="form-control input-lg" placeholder="请输入学号">
 						</div>
 
 						<div class="form-group">
 							<input type="password" name="password" id="password" class="form-control input-lg" placeholder="请输入密码">
 						</div>
 
-						<span class="button-checkbox sr-only">
-							<button type="button" class="btn" data-color="info">
-								记住
-							</button>
-							<input type="checkbox" name="remember_me" id="remember_me" checked="checked" class="hidden">
-						</span>
-
 						<div class="form-group">
 							<input id="login" type="submit" class="btn btn-default btn-lg btn-block" value="登录">
-						</div>
-
-						<div class="form-group">
-							<a href="./forgot-password.php" class="forgot-password pull-right">忘记密码</a>
 						</div>
 					</form>
 				</div>
@@ -265,12 +77,15 @@
 						</div>
 
 						<div class="form-group">
-							<input type="email" name="email-register" id="email-register" class="form-control input-lg" placeholder="请输入邮箱地址">
+							<input type="text" name="userid-register" id="userid-register" class="form-control input-lg" placeholder="请输入学号">
+						</div>
+						
+						<div class="form-group">
+							<input type="text" name="username-register" id="username-register" class="form-control input-lg" placeholder="请输入姓名">
 						</div>
 
 						<div class="form-group">
 							<input type="password" name="password-register" id="password-register" class="form-control input-lg" placeholder="请输入密码">
-							<span id="strength" class="low">低</span>
 						</div>
 
 						<div class="form-group">
@@ -283,6 +98,7 @@
 					</form>
 				</div>
 			</div>
+
 			
 		</div>
 
@@ -370,21 +186,23 @@
 		
 				var callback = "/nanolink/app/index.php";
 				$("#login").click(function() {
-					var email = $("#email").val();
-					if (!email) {
+					var userid = $("#userid").val();
+					if (!userid) {
 						$("#login-msg").css('visibility', 'visible');
-						$("#login-msg").html("用户名不能为空");
+						$("#login-msg").html("学号不能为空");
 						return false;
-					}
-					var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
-					if(!reg.test(email)) {
-						$("#login-msg").css('visibility', 'visible');
-						$("#login-msg").html("邮件地址不合法");
-						$("#email").focus();
-						return false;
+					}else{
+						$("#login-msg").css('visibility', 'hidden');
 					}
 					
 					var password = $("#password").val();
+					if (!password) {
+						$("#login-msg").css('visibility', 'visible');
+						$("#login-msg").html("密码不能为空");
+						return false;
+					}else{
+						$("#login-msg").css('visibility', 'hidden');
+					}
 			
 					$.ajax({
 						//type: $("#login-form").attr("method"),
@@ -418,31 +236,7 @@
 				// 验证密码强度
 				$("#password-register").keyup(function() {
 					var password = $("#password-register").val();
-			
-					if (password.length >= 6) {
-						$.ajax({
-							type: $("#register-form").attr("method"),
-							url: '../services/users/get-pwd-strength.php',
-							data: "password=" + password,
-					
-							success: function(res) {
-								$("#strength").css('display', 'block');
-								if (res >= 0 && res <= 2) {
-									$("#strength").attr('class', 'low');
-									$("#strength").html("低：尝试用大小写混合，以及特殊字符");
-									return false;
-								} else if (res >= 3 && res <= 5) {
-									$("#strength").attr('class', 'middle');
-									$("#strength").html("中：尝试用大小写混合，以及特殊字符");
-									return false;
-								} else {
-									$("#strength").attr('class', 'high');
-									$("#strength").html("高：请牢记您的密码");
-									return false;
-								}
-							}
-						});
-					} else {
+					if (password.length < 6) {
 						$("#strength").attr('class', 'red');
 						$("#strength").html("密码长度不能少于6位");
 					}
@@ -450,12 +244,12 @@
 		
 				// 提交表单
 				$("#register").click(function() {
-					var email = $("#email-register").val();
+					var userid-register = $("#userid-register").val();
 					var password = $("#password-register").val();
 					var confirmPassword = $("#confirm-password").val();
-					if (!password && !email) {
+					if (!password && !) {
 						$("#register-msg").css('visibility', 'visible');
-						$("#register-msg").html("邮件或密码不能为空");
+						$("#register-msg").html("学号或密码不能为空");
 						return false;
 					}
 			
@@ -508,5 +302,8 @@
 			  s.parentNode.insertBefore(hm, s);
 			})();
 		</script>
+
+		
+
 	</body>
 </html>
