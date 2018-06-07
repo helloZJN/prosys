e795083459dddce79b04007f6cfa503321b455b0
ZJN-PC <810863273@qq.com>
2018-06-07 22:48:21 +0800

    try-revert

--- checkid.php
+++ checkid.php
@@ -1,25 +1,29 @@
 <?php 
 	$useridRegister=$_POST['useridRegister'];
+	$userSelect=$_POST['userSelect'];
+	
 	header("Content-type:text/html;charset=utf-8");
 	include_once('conn/Oracle_oci.class.php');
 	$dbe=new Oracle_oci();
 	$dbe->conn();
 	if($dbe->conn){
-		//增加
-		//$dbe->insert("insert into admin values('我是你爸爸','5')");	
-		//更新	
-		//$dbe->update("update admin set  admid='666666' where admid='000000'");
-		//删除
-		//$dbe->delete('delete from admin');
-		//查找
-		$stid=$dbe->select("select * from admin");
-		while($row=oci_fetch_array($stid)){
-			foreach ($row as $value) {
-				echo "$value";
+		if($userSelect=="stu"){
+			$stid=$dbe->select("select * from student where stuid=$useridRegister");
+			if($row=oci_fetch_array($stid)){
+				echo "n";
+			}else{
+				echo "y";
+			}
+		}else{
+			$stid=$dbe->select("select * from teacher where teaid=$useridRegister");
+			if($row=oci_fetch_array($stid)){
+				echo "n";
+			}else{
+				echo "y";
 			}
 		}
 		$dbe->close();
 	}else {
-		echo "no";
+		echo "数据库连接失败";
 	}
  ?>
