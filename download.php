2a99dc7f8029cda0eb6925feb47b63effbbb3293
QPW <810863273@qq.com>
2018-06-12 13:34:08 +0800

    学生下载

--- download.php
+++ download.php
@@ -1,21 +1,23 @@
 <?php  
-$file_name = "xxx.rar";     //下载文件名    
-$file_dir = "./up/";        //下载文件存放目录    
-//检查文件是否存在    
-if (! file_exists ( $file_dir . $file_name )) {    
+include_once('conn/Oracle_oci.class.php');
+header("Content-type:text/html;charset=utf-8");
+session_start();
+
+$file_name = $_POST['file_name'];   
+$file_dir = $_POST['file_dir']; 
+$file_a= $file_dir ."/". $file_name;
+if (! file_exists ( iconv("utf-8", "gbk", $file_a) )) {    
     echo "文件找不到";    
     exit ();    
 } else {    
-    //打开文件    
-    $file = fopen ( $file_dir . $file_name, "r" );    
-    //输入文件标签     
+        
     Header ( "Content-type: application/octet-stream" );    
     Header ( "Accept-Ranges: bytes" );    
-    Header ( "Accept-Length: " . filesize ( $file_dir . $file_name ) );    
-    Header ( "Content-Disposition: attachment; filename=" . $file_name );    
-    //输出文件内容     
-    //读取文件内容并直接输出到浏览器    
-    echo fread ( $file, filesize ( $file_dir . $file_name ) );    
+    Header ( "Accept-Length: " . filesize ( iconv("utf-8", "gbk", $file_a)) );    
+    Header ( "Content-Disposition: attachment; filename=" . $file_name );  
+    $file = fopen (iconv("utf-8", "gbk", $file_a), "r" );  
+    readfile(iconv("utf-8", "gbk", $file_a));
+    //echo fread ( $file, filesize ( iconv("utf-8", "gbk", $file_a)) );    
     fclose ( $file );    
     exit ();    
 }    
