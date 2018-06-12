7fb09b7d56dd39fcc72ae3403eac9f07496c690a
ZJN-PC <810863273@qq.com>
2018-06-12 13:49:18 +0800

    tae1.0

--- tea_dir.php
+++ tea_dir.php
@@ -3,65 +3,60 @@
 		echo "<script>alert('你没有权限进入该页面！即将跳转到登陆界面');window.location.href='index.php';</script>";
 	}
 ?>
-<h1 class="page-header" id="pageheader">文件夹<button class="pull-right btn btn-info" onclick="get_dir(this)">创建文件夹</button><button class="pull-right btn btn-info" onclick="del_dir(this)">删除文件夹</button></h1>
-
-<div class="row">     
-	<div class="col-md-12">
-		<!--—panel面板，里面放置一些控件，下同---->
-		<div class="panel panel-primary">
-			<!--—panel面板的标题，下同---->
-			<div class="">
-				<div> <select class="form-control col-md-4" onchange="selectOnchang(this)">
-					<option value="" disabled selected></option>
-			            <?php 
+<?php echo "<a style='display:none' id='mkdir_id'>".$_SESSION['userid']."</a>"; ?>
+<div class="widget-head am-cf" >
+	<div class="widget-title am-fl">文件夹</div>
+</div>
+<div class="widget-body am-fr">
+	<div class="col-md-8" style="left:15%;">
+	<form class="am-form tpl-form-line-form" class="col-md-8">
+		<div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+        	<div class="am-input-group am-input-group-sm tpl-form-border-form cl-p">
+	            <input type="text" class="am-form-field " style="width: 200px" id="foldername">
+	            <span class="am-input-group-btn">
+	                <button class="am-btn  am-btn-default am-btn-success tpl-table-list-field am-icon-search" 
+	                onclick="make_dir(this)" type="button">创建新文件夹</button>
+	            </span>
+	        </div>
+	        <small>输入新文件夹名</small>
+	    </div>
+	    <div class="am-u-sm-12 am-u-md-12 am-u-lg-6">
+		    <div class="am-u-sm-12 am-u-md-6 am-u-lg-3">
+	            <div class="am-form-group tpl-table-list-select" >
+	                <select data-am-selected="{btnSize: 'sm'}" id="" style="width: 200px">
+		                <?php 
 			            	$dbe=new Oracle_oci();
 							$dbe->conn();
 							if($dbe->conn){
 								$stid=$dbe->select("select * from folder where teaid='".$_SESSION['userid']."'");
+
 								while($row=oci_fetch_array($stid)){
 									//var_dump($row);
 									echo '<option value="'.$row['0'].'">'.$row['1'].'</option>';
 								}
 								$dbe->close();
 							}
+				        ?>
+	                </select>
 
-			             ?>
-			        </select>
-			    </div>
-			       
-			</div>
-		</div>
-		<div class="col-md-12" id="stu_work">
-
-		</div>
-
+		            <small>选择文件夹</small> 
+		        </div>
+	        </div>
+	        <div class="am-u-sm-12 am-u-md-6 am-u-lg-6">
+                <div class="am-form-group">
+                    <div class="am-btn-toolbar">
+                        <div class="am-btn-group am-btn-group-s">
+                            <button type="button" class="am-btn am-btn-default am-btn-danger"><span class="am-icon-trash-o"></span> 删除</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+	    </div>
+	</form>
 	</div>
 </div>
-<div class="modal fade" id="makedir" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="padding-top: 50px">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">创建文件夹</h4>
-      </div>
-      <div class="modal-body">
-      	<form class="form-horizontal">
-		  <div class="form-group">
-		    <label for="usertype" class="col-sm-2 control-label">名称</label>
-		    <div class="col-sm-10">
-		      <input type="text" class="form-control" id="foldername" placeholder="文件夹名称">
-		    </div>
-		  </div>
-		</form>     
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-        <button type="button" class="btn btn-primary" onclick="make_dir(this)">创建</button>
-      </div>
-    </div>
-  </div>
-</div>
-<?php echo "<a style='display:none' id='mkdir_id'>".$_SESSION['userid']."</a>"; ?>
+
+
 <script type="text/javascript">
 	function selectOnchang(obj){
 		var value = obj.options[obj.selectedIndex].val();
@@ -77,13 +72,13 @@
 				}
 			}); 
 	}
-	function get_dir(e){
-		$('#makedir').modal({
-            keyboard: true
-        });
-	}
 	function make_dir(e){
 		var myDate = new Date();
+		if($("#foldername").val()==""){
+			+
+			alert("文件夹名不能为空");
+			return false;
+		}
 		$.ajax({
 				type: "POST",    
 				url: "teadir.php",    
